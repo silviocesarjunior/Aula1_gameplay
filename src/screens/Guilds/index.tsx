8789def2199fc9428daa2e824dd3f5e186f26c37
silviocesarjunior <78391096+silviocesarjunior@users.noreply.github.com>
2021-09-13 23:13:36 -0300

    improvements
    login improvements, using storage, modifications to guildsicons - screenshots brought from discord.

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,33 +1,36 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { View, FlatList } from 'react-native';
-import { Guild } from '../../components/Guild';
+import { Guild,GuildProps } from '../../components/Guild';
+import { Load } from '../../components/Load';
 import { styles } from './styles';
 import { ListDivider } from '../../components/ListDivider';
-import { GuildProps } from '../../components/Guild';
+import { useState } from 'react';
+import { api } from '../../services/api';
 
 type Props ={
     handleGuildSelect: (guild: GuildProps) => void;
 }
 
 export function Guilds({ handleGuildSelect}: Props){
-    const guilds = [
-    {
-        id: '1',
-        name: 'Lendários',
-        icon: 'image.png',
-        owner: true
-    },
-    {
-        id: '2',
-        name: 'Galera do Fifa',
-        icon: 'image.png',
-        owner: true
+    const [guilds, setGuilds] = useState<GuildProps[]>([]);
+    const [loading, setLoading] = useState(true);
+
+     async function fetchGuilds(){
+         const response = await api.get('/users/@me/guilds');
+
+         setGuilds(response.data);
+         setLoading(false);
     }
-];
+
+    useEffect( ()=> {
+        fetchGuilds();
+    },[]);
 
 return (
     <View style={styles.container}>
-        <FlatList
+        {
+            loading ? <Load /> : 
+            <FlatList
             data={guilds}
             keyExtractor={item => item.id}
             renderItem={({item}) => (
@@ -42,6 +45,7 @@ return (
             contentContainerStyle={{paddingBottom: 68, paddingTop: 103}}
             style={styles.guilds}
             />
+        }
     </View>
 )
 }
